53b4e4b256c3b9c80046791c9cd2841d21098235
Bill Farmer <williamjfarmer@yahoo.co.uk>
2016-11-27 19:36:36 +0000

    Add menu

--- src/main/java/org/billthefarmer/currency/Main.java
+++ src/main/java/org/billthefarmer/currency/Main.java
@@ -1,15 +1,58 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Currency - An android currency converter.
+//
+//  Copyright (C) 2013	Bill Farmer
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Bill Farmer	 william j farmer [at] yahoo [dot] co [dot] uk.
+//
+///////////////////////////////////////////////////////////////////////////////
+
 package org.billthefarmer.currency;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Main extends Activity
 {
-    /** Called when the activity is first created. */
+    // On create
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    // On create options menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+	// Inflate the menu; this adds items to the action bar if it
+	// is present.
+
+	MenuInflater inflater = getMenuInflater();
+	inflater.inflate(R.menu.main, menu);
+
+	return true;
     }
 }
