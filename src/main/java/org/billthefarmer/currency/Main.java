6cceb0b313cb1d64f5c894091ee3e9389c1f4349
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-04-14 20:27:19 +0100

    Add Icelandic krona

--- src/main/java/org/billthefarmer/currency/Main.java
+++ src/main/java/org/billthefarmer/currency/Main.java
@@ -92,8 +92,9 @@ public class Main extends Activity
         "NOK", "HRK", "RUB", "TRY",
         "AUD", "BRL", "CAD", "CNY",
         "HKD", "IDR", "ILS", "INR",
-        "KRW", "MXN", "MYR", "NZD",
-        "PHP", "SGD", "THB", "ZAR"
+        "ISK", "KRW", "MXN", "MYR",
+        "NZD", "PHP", "SGD", "THB",
+        "ZAR"
     };
 
     // Currency symbols
@@ -105,8 +106,8 @@ public class Main extends Activity
         "kr", "kn", "₽", "₺",
         "$", "R$", "$", "¥",
         "$", "Rp", "₪", "₹",
-        "₩", "$", "RM", "$",
-        "₱", "$", "฿", "S"
+        "kr", "₩", "$", "RM",
+        "$", "₱", "$", "฿", "S"
     };
 
     // Currency long names
@@ -120,9 +121,9 @@ public class Main extends Activity
         R.string.long_try, R.string.long_aud, R.string.long_brl,
         R.string.long_cad, R.string.long_cny, R.string.long_hkd,
         R.string.long_idr, R.string.long_ils, R.string.long_inr,
-        R.string.long_krw, R.string.long_mxn, R.string.long_myr,
-        R.string.long_nzd, R.string.long_php, R.string.long_sgd,
-        R.string.long_thb, R.string.long_zar
+        R.string.long_isk, R.string.long_krw, R.string.long_mxn,
+        R.string.long_myr, R.string.long_nzd, R.string.long_php,
+        R.string.long_sgd, R.string.long_thb, R.string.long_zar
     };
 
     // Currency flags
@@ -136,9 +137,9 @@ public class Main extends Activity
         R.drawable.flag_try, R.drawable.flag_aud, R.drawable.flag_brl,
         R.drawable.flag_cad, R.drawable.flag_cny, R.drawable.flag_hkd,
         R.drawable.flag_idr, R.drawable.flag_ils, R.drawable.flag_inr,
-        R.drawable.flag_kpw, R.drawable.flag_mxn, R.drawable.flag_myr,
-        R.drawable.flag_nzd, R.drawable.flag_php, R.drawable.flag_sgd,
-        R.drawable.flag_thb, R.drawable.flag_zar
+        R.drawable.flag_isk, R.drawable.flag_kpw, R.drawable.flag_mxn,
+        R.drawable.flag_myr, R.drawable.flag_nzd, R.drawable.flag_php,
+        R.drawable.flag_sgd, R.drawable.flag_thb, R.drawable.flag_zar
     };
 
     public static final String TAG = "Main";
