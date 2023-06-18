4e911ae915667ca3e55de50a3aa7603c8df70402
Bill Farmer <williamjfarmer@yahoo.co.uk>
2023-06-18 21:06:05 +0100

    Update version, changelog

--- src/main/java/org/billthefarmer/currency/CurrencyWidgetUpdate.java
+++ src/main/java/org/billthefarmer/currency/CurrencyWidgetUpdate.java
@@ -31,8 +31,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.util.Log;
